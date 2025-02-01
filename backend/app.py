9dcde22daa88e7b5abcc89eb30271f4a79512b7f
Abhinay-bir-17 <abhinaybirsingh10@gmail.com>
2025-02-01 20:03:42 +0530

    friends db is now empty, next we will create route to add a friend in db

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,35 @@
+# https://github.com/burakorkmez/react-python-tutorial/tree/master 
+# https://www.youtube.com/watch?v=tWHXaSC2T_s    ( 30:10 min )
+
+# do flask run to run app.py
+# app.py is the entry point to our app
+# used 'python app.py' to run in venv
+''' then to view http://127.0.0.1:5000/api/friends
+this gives error, so stop the server nd do this
+set FLASK_APP=app.py(no spaces around =)
+set FLASK_ENV = development
+nd then do ' flask run'
+'''
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS  
-
 app = Flask(__name__)
 CORS(app)
 
+# friends is our db name
 app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-dp = SQLAlchemy(app)
+db = SQLAlchemy(app)
+
+# as routes aint returin anything so we dont need to say from
+# import routes 
+import routes 
+#  do the below to create a db, then do flask run
+with app.app_context():
+  db.create_all() #creates columns of db
+
+# below means when we run this file then only run app, it prevents
+# app.run to execute if it has been imported by other files
 if __name__=="__main__":
     app.run(debug=True)
-
-#  at 26:03 seconds
--- backend/routes.py
+++ backend/routes.py
@@ -1,3 +1,14 @@
 from app import app,db
 from flask import request, jsonify
 from models import Friend
+
+# Get all friends
+@app.route("/api/friends",methods=["GET"])
+def get_friends():
+  '''below  we didnt write select * from friends '''
+  ''' friends will be in python objects representation '''
+  friends = Friend.query.all() 
+  result = [friend.to_json() for friend in friends] # list comprehension
+  # result will look like :,[{...}, {...} ....]
+  return jsonify(result) #status code is 200 by default
+
